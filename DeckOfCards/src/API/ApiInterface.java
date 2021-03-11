ad427bb45c3fb01730c2a6db1f949b904c30f1b9
atraylo <77689642+atraylo@users.noreply.github.com>
2021-03-11 05:06:23 -0500

    Add files via upload
    this is the basic structure of code. I based it off of the past API example he had given. Its not the best.

---
+++ DeckOfCards/src/API/ApiInterface.java
@@ -0,0 +1,13 @@
+package API;
+/**
+ * This API interface defines the methods that need to be implemented for any
+ * valid API Connector translator class.
+ *
+ *
+ */
+
+public interface ApiInterface {
+
+    public String Shuffle(int _cards);
+
+}
