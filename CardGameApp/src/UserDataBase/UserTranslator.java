534bcaa4b20e454fa5a46bd60610995676178a8e
Serena Wisnewski Herter <69825892+SerenaWisHerter@users.noreply.github.com>
2021-04-24 17:16:15 -0400

    added game to view connector and empty classes
    added empty DB translator and connector classes
    War Game still has issues with moving from one pile to another

---
+++ CardGameApp/src/UserDataBase/UserTranslator.java
@@ -0,0 +1,12 @@
+/*
+ * The Four Lokos
+ * Created by: 
+ * Created on: 
+ * This class translates the user information from the database
+ */
+package UserDataBase;
+
+public class UserTranslator {
+    
+    
+}
